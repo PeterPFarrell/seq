71cf1f979f3c53573c5fba3f55e9e8d9d82a5299
PeterPFarrell <222321069+PeterPFarrell@users.noreply.github.com>
2026-05-19 16:13:08 +0100

    fixed command

--- src/main/kotlin/seq/services/StatusService.kt
+++ src/main/kotlin/seq/services/StatusService.kt
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Service
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-const val CONTAINER_CMD = "docker compose ps"
+const val CONTAINER_CMD = listOf("docker","compose","ps")
 
 @Service
 class StatusService {
 
     fun getContainerInfo() : String {
-        return runCommand(listOf(CONTAINER_CMD))
+        return runCommand(CONTAINER_CMD)
     }
 
 
