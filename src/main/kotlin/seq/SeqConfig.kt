bbb2a1fd851e5a81cc454e03d777c021ca021010
PeterPFarrell <222321069+PeterPFarrell@users.noreply.github.com>
2026-05-24 18:48:13 +0100

    added environments

--- src/main/kotlin/seq/SeqConfig.kt
+++ src/main/kotlin/seq/SeqConfig.kt
@@ -1,17 +1,30 @@
 package seq
 
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 
+@ConfigurationProperties(prefix = "app.cors")
+data class CorsProps(
+    val allowed: List<String> = emptyList()
+)
 
-
-
+@EnableConfigurationProperties(CorsProps::class)
 @Configuration
-class SeqConfig : WebMvcConfigurer {
+class SeqConfig(
+    private val cors: CorsProps
+) : WebMvcConfigurer {
+    private val logger = LoggerFactory.getLogger(javaClass)
     override fun addCorsMappings(registry: CorsRegistry) {
+        logger.info("Adding cors mappings: {}", cors.allowed.joinToString(","))
+
+        if (cors.allowed.isEmpty() || cors.allowed == null) return
+
         registry.addMapping("/**")
-            .allowedOriginPatterns("https://*.sequesterapp.com", "https://sequesterapp.com", "http://localhost:8080")
+            .allowedOriginPatterns(*cors.allowed.toTypedArray())
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
     }
