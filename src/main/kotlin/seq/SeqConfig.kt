bafef269a798c49e8da04f0824690aa69b280d8a
PeterPFarrell <222321069+PeterPFarrell@users.noreply.github.com>
2026-05-24 17:44:46 +0100

    allow origin from local env

--- src/main/kotlin/seq/SeqConfig.kt
+++ src/main/kotlin/seq/SeqConfig.kt
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class SeqConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("https://*.sequesterapp.com", "https://sequesterapp.com")
+            .allowedOriginPatterns("https://*.sequesterapp.com", "https://sequesterapp.com", "http://localhost:8080")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
     }
